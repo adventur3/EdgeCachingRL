916ea588eeac7679bda4925719ad8c09e9aec0f7
adventur3 <wydn12345@qq.com>
2021-01-08 19:27:49 +0800

    update

--- caching_env.py
+++ caching_env.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import time
 
 RSU_NUM = 4
@@ -12,6 +13,7 @@ class CachingEnv:
         self.cache_state = np.zeros((RSU_NUM, REQUEST_NUM))
         self.region_request = np.zeros((REGION_NUM, REQUEST_NUM))
         self.rsu_connect = np.zeros((RSU_NUM, RSU_NUM))
+        self.region_rsu = np.zeros(REGION_NUM)
 
         self.rsu_capcity = np.zeros(RSU_NUM)
         self.rsu_residual_capcity = np.zeros(RSU_NUM)
@@ -33,18 +35,46 @@ class CachingEnv:
         calculatePopularity()
         if action == 0: #global popular content in core rsu
             popularId = np.argmax(self.request_popularity)
-            if cache_state[self.index_of_core][popularId] == 0 && self.rsu_residual_capcity >= self.request_size[popularId]:
-                cache_state[self.index_of_core][popularId] = 1
+            if self.cache_state[self.index_of_core][popularId] == 0 and self.rsu_residual_capcity >= self.request_size[popularId]:
+                self.cache_state[self.index_of_core][popularId] = 1
                 self.rsu_residual_capcity -= self.request_size[popularId]
+
         elif action == 1: #current popular content in local rsu
             currentContentPopularity = {}
             for i in range(REQUEST_NUM):
                 currentContentPopularity[i] = 0
-            
+            for i in range(REGION_NUM):
+                for j in range(REQUEST_NUM):
+                    if self.region_request[i][j] == 1:
+                        currentContentPopularity[j] += 1
+            popularId = max(currentContentPopularity,key=currentContentPopularity.get)
+            tempRequest = self.region_request[:,popularId]
+            for i in range(REGION_NUM):
+                if tempRequest[i] == 1:
+                    tempRSUId = self.region_rsu[i]
+                    if self.cache_state[tempRSUId][popularId] == 0 and self.rsu_residual_capcity[tempRSUId]>=self.request_size[popularId]:
+                        self.cache_state[tempRSUId][popularId] = 1
+                        self.rsu_residual_capcity[tempRSUId] -= self.request_size[popularId]
+
         elif action == 2: #current popular content in core rsu
+            currentContentPopularity = {}
+            for i in range(REQUEST_NUM):
+                currentContentPopularity[i] = 0
+            for i in range(REGION_NUM):
+                for j in range(REQUEST_NUM):
+                    if self.region_request[i][j] == 1:
+                        currentContentPopularity[j] += 1
+            popularId = max(currentContentPopularity, key=currentContentPopularity.get)
+            if self.cache_state[self.index_of_core][popularId] == 0 and self.rsu_residual_capcity[self.index_of_core] >= self.request_size[popularId]:
+                self.cache_state[self.index_of_core][popularId] = 1
+                self.rsu_residual_capcity[self.index_of_core] -= self.request_size[popularId]
 
-        else: #random cache
-
+        else:  #cache
+            tempRSUId = random.randint(0,RSU_NUM-1)
+            tempRequestId = random.randint(0,REQUEST_NUM-1)
+            if self.cache_state[tempRSUId][tempRequestId] == 0 and self.rsu_residual_capcity[tempRSUId] >= self.request_size[tempRequestId]:
+                self.cache_state[tempRSUId][tempRequestId] = 1
+                self.rsu_residual_capcity[tempRSUId] -= self.request_size[tempRequestId]
 
 
         trans_efficiency_sum = 0
